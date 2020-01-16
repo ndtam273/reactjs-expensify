332eb9c982bd99a6e559458dac750cac0b51a60b
tam nguyen <ndtam.273@gmail.com>
2020-01-16 21:43:35 +0700

    no message

---
+++ src/playground/destructuring.js
@@ -0,0 +1,23 @@
+const person = {
+    name: 'andrew',
+    age: 26,
+    location: {
+        city: 'Newyork',
+        temp: 92
+    }
+};
+
+//destruction
+const {name, age} = person;
+console.log(`${person.name} is ${person.age}`);
+
+const { city, temp: temperature} = person.location;
+
+if (city && temperature) {
+    console.log(`It's ${temperature} in ${city}`)
+}
+
+//------------------Array destructuring--------------
+
+const address = ['1299 S jupiter street', 'philadenphia', 'pennysilia', '19147'];
+console.log(`You are in ${}`);
