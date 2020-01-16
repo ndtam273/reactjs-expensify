70b7882ee1ad4c8cadfe18c6ba7cc00dd7e9bfec
tam nguyen <ndtam.273@gmail.com>
2020-01-16 17:48:51 +0700

    redux-expensify -> on doing

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,21 +1,64 @@
-import { createStore, combineReducers} from 'redux';
+import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 // ADD_EXPENSE
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0 } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
 // REMOVE_EXPENSE
+// const removeExpense = () => ({
+//     type: 'REMOVE_EXPENSE'
+// });
 // EDIT_EXPENSE
+// const editExpense = () => ({
+//     type: 'ADD_EXPENSE'
+// });
 // SET_TEXT_FILTER
+// const addExpense = () => ({
+//     type: 'ADD_EXPENSE'
+// });
 // SORT_BY_DATE
+// const addExpense = () => ({
+//     type: 'ADD_EXPENSE'
+// });
 // SORT_BY_AMOUNT
+// const addExpense = () => ({
+//     type: 'ADD_EXPENSE'
+// });
 // SET_START_DATE
+// const addExpense = () => ({
+//     type: 'ADD_EXPENSE'
+// });
 // SET_END_DATE
+// const addExpense = () => ({
+//     type: 'ADD_EXPENSE'
+// });
 
 // Expenses reducer
 
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
-   switch (action.type) {
-       default:
-           return state;
-   }
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+               ...state, 
+               action.expense
+            ];
+        default:
+            return state;
+    }
 };
 
 // Filter Reducer
@@ -32,13 +75,20 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     }
 };
 // store creation
-const store = createStore( 
+const store = createStore(
     combineReducers({
         expenses: expensesReducer,
         filters: filtersReducer
     })
 );
-console.log(store.getState());
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+store.dispatch(addExpense({ description: 'Coffee', amount: 300}));
+
 const demoState = {
     expenses: [{
         id: '0294809fsf',
