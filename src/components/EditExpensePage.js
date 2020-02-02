1fc8a61bae516bcbd131e44f721aba3af3f32495
tam nguyen <ndtam.273@gmail.com>
2020-02-02 21:18:19 +0700

    edit form

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from './ExpenseForm';
 
 const EditExpensePage = (props) => {
-    console.log(props);
     return (
         <div>
-        Edit the expense with id of {props.match.params.id}
+        <ExpenseForm
+          onSubmit={(expense) => {
+             console.log('update', expense);
+          }
+
+          }
+        />
       </div>
     );
     
     }; 
+    const mapStateToProps = (state, props) => {
+       return {
+         expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+       };
+    };
 
-export default EditExpensePage;
+export default connect(mapStateToProps)(EditExpensePage);
