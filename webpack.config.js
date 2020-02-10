78a13523a5c7fb4c15407eadb390bb325abda922
tam nguyen <ndtam.273@gmail.com>
2020-02-10 07:30:14 +0700

    css file separate

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = (env) => {
-  const isProduction = env === 'production';
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
-  console.log('env', env);
+  console.log("env", env);
   return {
     entry: "./src/app.js",
     output: {
@@ -19,11 +21,16 @@ module.exports = (env) => {
         },
         {
           test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"]
+          use: CSSExtract.extract({
+            use: ["css-loader", "sass-loader"]
+          })
         }
       ]
     },
-    devtool: isProduction? "source-map" : "cheap-module-eval-source-map",
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true
