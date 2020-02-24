e9833daf580ee529864fede9b7ee64a6280ec0fc
tam nguyen <ndtam.273@gmail.com>
2020-02-24 17:40:50 +0700

    firebase with redux

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,76 +1,55 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAnMDKUX54AILq893cpy4dYPq93Hr45ufw",
-    authDomain: "expensify-19ed7.firebaseapp.com",
-    databaseURL: "https://expensify-19ed7.firebaseio.com",
-    projectId: "expensify-19ed7",
-    storageBucket: "expensify-19ed7.appspot.com",
-    messagingSenderId: "248026656408",
-    appId: "1:248026656408:web:5bffb2cfc4b017b0822aed"
-  };
+  apiKey: "AIzaSyAnMDKUX54AILq893cpy4dYPq93Hr45ufw",
+  authDomain: "expensify-19ed7.firebaseapp.com",
+  databaseURL: "https://expensify-19ed7.firebaseio.com",
+  projectId: "expensify-19ed7",
+  storageBucket: "expensify-19ed7.appspot.com",
+  messagingSenderId: "248026656408",
+  appId: "1:248026656408:web:5bffb2cfc4b017b0822aed"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  const database = firebase.database();
+const database = firebase.database();
 
-  // database.ref()
-  //     .once('value')
-  //     .then((snapshot) => {
-  //       const val = snapshot.val();
-  //       console.log(val);
-  //     })
-  //     .catch(() => {
-  //       console.log('Error fetching data');
-  //     });
-database.ref().on('value', (snapshot) => {
-  console.log(snapshot.val());
-});
+export  { firebase, database as default };
 
-setTimeout(() => {
-  database.ref('age').set(28);
-}, 3500);
+// child remove
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+// child change
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-//   database.ref().set({
-//      name: 'Tam Nguyen',
-//      age: 30,
-//      job: {
-//        title: '侍',
-//        company: '帝国'
-//      },
-//      stressLevel: 6,
-//      isSingle: true,
-//      location: {
-//          city: 'DaNang',
-//          country: 'VietNam'
-//      }
-//   }).then(() => {
-//       console.log('data has been saved!');
-//   }).catch((e) => {
-//       console.log('this failed', e);
+// child add
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+
+//
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
 //   });
 
-// //   database.ref().set('This is my data.');
-// database.ref('age').set(32);
-// database.ref('location/city').set('HaNoi');
-// database.ref('attributes').set ( {
-//     height: 165,
-//     weight: 62
-// }).then(() => {
-//     console.log('Second set call worked');
-// }).catch((e) => {
-//     console.log('This did not work', e);
-// });
+  // database.ref('expensives').push(
+  //   {
+  //     description: 'Rent' ,
+  //     note: '',
+  //     amount: 109500,
+  //     createdAt: 976123498763
+  //   }
+  // );
 
-// database.ref('isSingle').remove().then(() => {
-//     console.log('Removed!');
-// }).catch((e) => {
-//     console.log('Remove did not workd', e);
-// });
-
-// database.ref().update({
-//    stressLevel: 9,
-//    'job/company': 'Kingdom',
-//    'location/city': 'Japan'
-    
-// });
