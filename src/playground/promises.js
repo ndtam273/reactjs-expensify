b9c5b8ac75835c8cbe6e739a4fcd893a6a089e91
tam nguyen <ndtam.273@gmail.com>
2020-02-25 14:06:44 +0700

    setup test database

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,21 +1,27 @@
 const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({
+      name: 'Andrew',
+      age: 26
+    });
+    // reject('Something went wrong!');
+  }, 5000);
+});
+
+console.log('before');
+
+promise.then((data) => {
+  console.log('1', data);
+
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: 'Tam',
-        //     age: 30
-        // });
-        reject('Something went wrong');
-    }, 1500);
-
+      resolve('This is my other promise');
+    }, 5000);
+  });
+}).then((str) => {
+  console.log('does this run?', str);
+}).catch((error) => {
+  console.log('error: ', error);
 });
 
-
-promise.then((data) => {
-    console.log('1', data);
-});
-
-promise.then((data) => {
-    console.log('2', data);
-});
-
-
+console.log('after');
